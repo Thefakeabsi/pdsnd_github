bb383e906ce024dfe51704d65dbe91beb12059f5
Daniel Choi <dchoi3810@gmail.com>
2020-05-16 17:47:16 -0400

    edited time_stats function and added format method in strings

--- bikeshare.py
+++ bikeshare.py
@@ -105,24 +105,24 @@ def time_stats(df, month, day):
         common_month = df['month'].mode()[0]
         month_list = ['January', 'February', 'March', 'April', 'May', 'June']
         common_month = month_list[common_month - 1]
-        print_message(f"The most common month to travel is {common_month.title()}.")
+        print_message("The most common month to travel is {}.".format(common_month.title()))
     else:
-        print_message(f'The selected data is filtered by {month.title()}.')
+        print_message('The selected data is filtered by {}.'.format(month.title()))
     # Displays the most common day of week
     if day == 'all':
         common_day = df['day_of_week'].mode()[0]
-        print_message(f"The most common day of the week to travel is {common_day}.")
+        print_message("The most common day of the week to travel is {}.".format(common_day))
     else:
-        print_message(f"The selected data is filtered by {day}.")
+        print_message("The selected data is filtered by {}.".format(day))
 
     # Displays the most common start hour
     df['hour'] = df['Start Time'].dt.hour
     common_hour = df['hour'].mode()[0]
     if common_hour > 12:
         common_hour -= 12
-        print_message(f"The most common start hour is {common_hour} o'clock PM.")
+        print_message("The most common start hour is {} o'clock PM.".format(common_hour))
     else:
-        print_message(f"The most common start hour is {common_hour} o'clock AM.")
+        print_message("The most common start hour is {} o'clock AM.".format(common_hour))
     print("\nThis took %s seconds." % (time.time() - start_time))
     print_message('-'*40)
 
